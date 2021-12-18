d482dbce186ca0da4af0500bf3f61299ed997604
Jessy Pango <kvnjessy@gmail.com>
2021-12-18 08:40:44 +0100

    Test d'hibernate-search réusie

--- src/main/java/tn/ipsas/gestconv/servlet/InitServlet.java
+++ src/main/java/tn/ipsas/gestconv/servlet/InitServlet.java
@@ -1,20 +1,25 @@
 package tn.ipsas.gestconv.servlet;
 
-import java.io.IOException;
+import org.hibernate.search.mapper.orm.Search;
+import org.hibernate.search.mapper.orm.massindexing.MassIndexer;
+import org.hibernate.search.mapper.orm.session.SearchSession;
+import tn.ipsas.gestconv.bean.Convention;
 
-import javax.servlet.ServletConfig;
+import javax.persistence.EntityManager;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 
-import tn.ipsas.gestconv.db.connection.DatabaseConnectionFactory;
-
-@WebServlet(value="/initServlet", name = "init", loadOnStartup=1)
+@WebServlet(value = "/initServlet", name = "init", loadOnStartup = 1)
 public class InitServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-	
+    private static final long serialVersionUID = 1L;
+
     public InitServlet() {
         super();
     }
 
+    @Override
+    public void init() throws ServletException {
+        // Indexage initiale par hibernate search
+    }
 }
